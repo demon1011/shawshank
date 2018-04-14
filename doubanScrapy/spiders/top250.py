cde6aca20564220b5fd051acf26af709bfe0da3f
demon1011 <184380405@qq.com>
2018-04-14 13:37:50 +0800

    v0.1

--- doubanScrapy/spiders/top250.py
+++ doubanScrapy/spiders/top250.py
@@ -12,9 +12,8 @@ class ShawScrapy(Spider):
 	
 	def parse(self,response):
 		for theme in response.xpath('//ol[@class="grid_view"]/li'):
-			movie_page=theme.xpath('div[@class="item"]/div[class="pic"]/a/@href').extract()
-			if movie_page:
-				movie_url=str(str(movie_page[0])+'reviews')
+			movie_page=theme.xpath('div[@class="item"]/div[@class="pic"]/a/@href').extract_first()
+				movie_url=movie_page+'reviews'
 				yield Request(movie_url,callback=self.parse_review)
 			
 
